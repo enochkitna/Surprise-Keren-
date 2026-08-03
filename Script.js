160972699489a8cbf3baf5a0cccbf8e75d9649a5
enochkitna <enochkitna@gmail.com>
2026-08-03 12:30:12 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "❤️ Keren, tu es une personne extraordinaire. Merci d'être dans ma vie. Je t'aime plus que les mots ne peuvent l'exprimer. ❤️";
+}
